f6672b346a112a6fe210333e877500305f0e1ca3
Gustavo Santos Teixeira <gustavosantosteixeira98@gmail.com>
2025-12-19 13:06:24 -0300

    first

---
+++ s.sh
@@ -0,0 +1,11 @@
+sudo apt install texlive-full;
+latexmk -pdf arquivo.tex;
+ rm arquivo.aux;
+ rm arquivo.bbl;
+ rm arquivo.blg;
+ rm arquivo.fls;
+ rm arquivo.log;
+ rm arquivo.out;
+ rm arquivo.fdb_latexmk;
+
+
